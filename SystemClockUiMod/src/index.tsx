16b17627a552477793f313a0f549194ba48175ea
liberaldev <ssharp0614@gmail.com>
2025-04-20 03:25:09 +0900

    당장은 필요없는 로그 주석처리

--- SystemClockUiMod/src/index.tsx
+++ SystemClockUiMod/src/index.tsx
@@ -1,11 +1,12 @@
 import { ModRegistrar } from "cs2/modding";
-import { SystemTime } from "./system-time";
+
+import {SystemTime} from "./system-time";
 
 const register: ModRegistrar = (moduleRegistry) => {
-    console.log(
+    /*console.log(
         'List of everything toolbar',
         moduleRegistry.find(/toolbar/)
-    );
+    );*/
     moduleRegistry.extend('game-ui/game/components/toolbar/bottom/happiness-field/happiness-field.tsx', 'HappinessField', SystemTime);
 }
 
